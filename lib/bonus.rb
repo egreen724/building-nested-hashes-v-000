3bee26441fd02cece7814d48ca705590cce825a2
Erin Green <erin.y.green@gmail.com>
2018-09-24 19:01:50 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,9 @@ def bonus
   #code your solution here:
 
   
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+def bonus 
+  epic_tragedy[:hero][:status] = "dead"
+  epic_tragedy[:heroine][:status] = "dead"
+  
   epic_tragedy
 end
